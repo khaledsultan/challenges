a194d0b95c434af5d853ab22a93910d0023fe71d
Khaled sultan <eng-sultan.k@hotmail.com>
2023-05-26 16:56:55 +0200

    js

--- dark-mode/js/index.js
+++ dark-mode/js/index.js
@@ -1,3 +1,29 @@
 console.clear();
 
-const bodyElement = document.querySelector('[data-js="body"]');
+const mainElement = document.querySelector('[data-js="body"]');
+
+const addDarkMode = document.querySelector('[data-js="dark-mode-button"]');
+addDarkMode.addEventListener("click", () => {
+    mainElement.classList.add("dark");
+    // mainElement.classList.remove("light");
+    // mainElement.classList.add("dark");
+    
+  });
+
+  const addLightMode = document.querySelector('[data-js="light-mode-button"]');
+  addLightMode.addEventListener("click", () => {
+      mainElement.classList.remove("dark");
+    // mainElement.classList.remove("dark");
+    // mainElement.classList.add("light");
+  });
+
+  
+  const addToggleMode = document.querySelector('[data-js="toggle-button"]');
+  addToggleMode.addEventListener("click", () => {
+      mainElement.classList.toggle("dark");
+    // mainElement.classList.remove("dark");
+    // mainElement.classList.remove("light");
+    // mainElement.classList.add("normal");
+
+    
+  });
