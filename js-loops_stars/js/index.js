84c1441b7092f487e73abaee54bdd6451c8ee5bd
Khaled sultan <eng-sultan.k@hotmail.com>
2023-06-08 13:11:22 +0200

    js

--- js-loops_stars/js/index.js
+++ js-loops_stars/js/index.js
@@ -5,20 +5,23 @@ const starContainer = document.querySelector('[data-js="star-container"]');
 function renderStars(filledStars=0) {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
-  for (let i = 0; i < 5; i++) {
-    const imgs = document.createElement("img");
+  for (let i = 1; i < 6; i++) {
+    
+    const newStar = document.createElement("img");
+    starContainer.append(newStar);
     if (i <=  filledStars ) {
       
-      imgs.src = "assets/star-filled.svg"
+      newStar.src = "assets/star-filled.svg"
      
     } else {
       
-      imgs.src = "assets/star-empty.svg"
+      newStar.src = "assets/star-empty.svg"
       
     }
-  
-    starContainer.append(imgs);
-    
+    newStar.addEventListener("click", () => {
+    renderStars(i);});
+    starContainer.appendChild(newStar);
+ 
   }
 
 }
