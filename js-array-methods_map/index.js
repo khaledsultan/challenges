47ff813c08dfe000bea3b490ffbe997bf23cbd0b
Khaled sultan <eng-sultan.k@hotmail.com>
2023-06-09 07:07:02 +0200

    js

---
+++ js-array-methods_map/index.js
@@ -0,0 +1,36 @@
+console.clear();
+
+const cards = [
+  {
+    id: "1",
+    isBookmarked: false,
+    question: "How often can I use <header>?",
+    answer: "As often as you like.",
+    tags: ["html", "elements", "basic"],
+  },
+  {
+    id: "2",
+    isBookmarked: false,
+    question: "How often can I use <aside>?",
+    answer: "As often as you like.",
+    tags: ["html", "elements", "basic"],
+  },
+  {
+    id: "3",
+    isBookmarked: true,
+    question: "On which types can I use destructuring assignment?",
+    answer: "On Objects and Arrays",
+    tags: ["js", "next", "advanced"],
+  },
+];
+
+// const lowerCaseAnswers = cards.map((card) => {
+//   return card.toUpperCase();
+// }); // ['as often as you like.', ...]
+const lowerCaseAnswers = cards.forEach()
+console.log(lowerCaseAnswers);
+const questionsAndAnswersTogether = null; // ["How often can I use <header>? - As often as you like.", ...]
+
+const questionAndAnswer = null; // [{ question: 'How often can I use <header>?', answer: 'As often as you like.'}, {...}]
+
+export { lowerCaseAnswers, questionsAndAnswersTogether, questionAndAnswer };
