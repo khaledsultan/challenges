6ff134deffc2d454eaacbc0f2eb254248643ec23
Khaled sultan <eng-sultan.k@hotmail.com>
2023-06-09 12:08:14 +0200

    fs

---
+++ fs-linting-basics-template-challange/index.js
@@ -0,0 +1,15 @@
+function toggleButton() {
+  const cards = document.querySelectorAll('[data-js=card]');
+  // const buttons = document.querySelectorAll('[data-js=buttons]');
+  cards.forEach(card => {
+    const button = card.querySelector('[data-js="button"]');
+    const link = card.querySelector('[data-js="link"]');
+    const arrow = card.querySelector('[data-js="arrow"]');
+    button.addEventListener('click', () => {
+      link.classList.toggle('hide');
+      arrow.classList.toggle('rotate');
+    });
+  });
+}
+
+toggleButton();
