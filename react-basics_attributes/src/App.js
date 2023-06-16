61655b947fb63ddfca3bc46c2d748c79d04256a4
Khaled sultan <eng-sultan.k@hotmail.com>
2023-06-16 15:19:36 +0200

    react

---
+++ react-basics_attributes/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <article>
+      <h2 className="article__title">Hello World !</h2>
+      <a
+        className="article__link"
+        inputMode=""
+        aria-label=""
+        id=""
+        href="https://de.wikipedia.org/wiki/React"
+      >
+        React wikipedia
+      </a>
+    </article>
+  );
+}
