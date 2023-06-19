82d0d45b94f97cbe7ef4a3871775b60f80dd5393
Khaled sultan <eng-sultan.k@hotmail.com>
2023-06-19 15:19:22 +0200

    childern

---
+++ react-nesting_buttons-and-children/src/App.js
@@ -0,0 +1,21 @@
+import React, { Children } from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <main>
+      <Button>1</Button>
+      <Button>2</Button>
+      <Button>3</Button>
+      <Button>4</Button>
+    </main>
+  );
+}
+
+function Button({ children }) {
+  return (
+    <button className="button" type="button">
+      {children}
+    </button>
+  );
+}
