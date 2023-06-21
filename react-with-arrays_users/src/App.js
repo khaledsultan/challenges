840842dc530261c4ce5c334d47d244e815831d69
Khaled sultan <eng-sultan.k@hotmail.com>
2023-06-21 13:24:27 +0200

    array

--- react-with-arrays_users/src/App.js
+++ react-with-arrays_users/src/App.js
@@ -9,7 +9,12 @@ export default function App() {
     <main className="app">
       <Title text="ClientBoard" />
       <div className="app__card-grid">
-        <Card user={USERS[0]} />
+        {USERS.map((user) => (
+          // console.log(user)
+          <Card key={user.id} user={user} />
+
+        ))}
+        
       </div>
     </main>
   );
--- react-with-arrays_users/src/components/Tag.js
+++ react-with-arrays_users/src/components/Tag.js
@@ -1,5 +1,8 @@
 import "./Tag.css";
 
 export default function Tag({ tag }) {
-  return <li className="tag">{tag}</li>;
+  const a = tag === "admin"?"tag--highlight" : null;
+  return <li className={`tag ${a}`}>{tag}</li>;
 }
+
+
