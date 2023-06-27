49930bffd72e8a87cedee40e107f267dd5bb776b
Khaled sultan <eng-sultan.k@hotmail.com>
2023-06-27 13:27:41 +0200

    journalApp

--- journal/src/components/Entry/index.js
+++ journal/src/components/Entry/index.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { ReactComponent as Star } from "../imges/star.svg";
+import { ReactComponent as FilledStar } from "../imges/star-filled.svg";
 
-export default function Entry({ date, title, text }) {
+export default function Entry({ key, date, motto, notes }) {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  function handleToggle() {
+    setIsFavorite(!isFavorite);
+  }
+
+
   return (
-    <>
-      <section className="Entry">
-        <p>{date}</p>
-        <h3>
-          <strong>"{title}"</strong>
+        <>
+        <section className="Entry">
+         <p>{date}</p>
+         <h3>
+        <strong>"{motto}"</strong>
         </h3>
-        <Star />
-        <p>{text}</p>
+        {isFavorite ? (
+        <FilledStar onClick={handleToggle} />
+      ) : (
+        <Star onClick={handleToggle} />
+        )}
+        <p>{notes}</p>
         <hr />
+      
       </section>
     </>
   );
 }
+
--- journal/src/components/main/index.js
+++ journal/src/components/main/index.js
@@ -27,7 +27,12 @@ export default function Main() {
         <Counter type="All Entires" number="3" className="Counter"></Counter>
         <Counter type="Favorites" number="1" className="Counter"></Counter>
       </section>
-      <Entry
+      <>
+     {entries.map(({ id, date,motto,notes }) => (
+       <Entry key={id} date={date} motto={motto}  notes={notes}/>
+     ))}
+   </>
+       {/* <Entry
         
         date="FEB 27,2028"
         title="That's life in the city"
@@ -38,7 +43,39 @@ export default function Main() {
         date="FEB 26,2028 "
         title="That's life in the city"
         text=" sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nu"
-      ></Entry>
+      ></Entry>  */}
+
     </>
   );
 }
+
+
+const entries = [
+  {
+    id: 1000,
+    date: "Feb 5, 2025",
+    motto: "We are in a state of chaos",
+    notes:
+      "Today I learned about React State. It was fun! I can't wait to learn more.",
+  },
+  {
+    id: 999,
+    date: "Feb 4, 2025",
+    motto: "Props, Props, Props",
+    notes:
+      "Today I learned about React Props. Mad props to everyone who understands this!",
+  },
+  {
+    id: 998,
+    date: "Feb 3, 2025",
+    motto: "How to nest components online fast",
+    notes:
+      "Today I learned about React Components and how to nest them like a pro. Application design is so much fun!",
+  },
+  {
+    id: 997,
+    date: "Feb 2, 2025",
+    motto: "I'm a React Developer",
+    notes: "My React-ion when I learned about React: 😍",
+  },
+];
